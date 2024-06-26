61c1cb704608ad68470e56f809a09be1b89fa0d6
Daniel Núnez Rojas <invdesdelnr@outlook.com>
2024-06-26 09:29:15 -0400

    partida practica, primer commit

---
+++ src/main/java/controllers/UsuarioController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value = "prueba")
+    public List<String> prueba(){
+
+
+        return List.of("manzana","kiwi", "Banan");
+    }
+}
