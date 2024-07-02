ee0cf1cef2a0801a2b6e826900f371f86233993d
Daniel Núnez Rojas <invdesdelnr@outlook.com>
2024-07-02 15:49:15 -0400

    primer commit

--- src/main/java/com/practicaJava/practica/controllers/UsuarioController.java
+++ src/main/java/com/practicaJava/practica/controllers/UsuarioController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
     @RequestMapping(value = "usu/{id}")
-
     public Usuario getUsuario(@PathVariable long id){
-
         Usuario usu = new Usuario();
         usu.setId(id);
         usu.setNombre("Daniel");
@@ -20,6 +21,37 @@ public class UsuarioController {
         usu.setEmail("invdesdev@outlook.com");
         usu.setTelefono("9410333333");
         return usu;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usu = new Usuario();
+        usu.setId(234L);
+        usu.setNombre("Daniel");
+        usu.setApellido("Nuñez");
+        usu.setEmail("invdesdev@outlook.com");
+        usu.setTelefono("9410333333");
+
+        Usuario usu2 = new Usuario();
+        usu2.setId(456L);
+        usu2.setNombre("Migul");
+        usu2.setApellido("hay MIGUEL");
+        usu2.setEmail("invdesdevc@outlook.com");
+        usu2.setTelefono("941033213");
+
+        Usuario usu3 = new Usuario();
+        usu3.setId(678L);
+        usu3.setNombre("Demente");
+        usu3.setApellido("Nuñez");
+        usu3.setEmail("invdesdevf@outlook.com");
+        usu3.setTelefono("71928030");
+
+        usuarios.add(usu);
+        usuarios.add(usu2);
+        usuarios.add(usu3);
+
+        return usuarios;
     }
 
     @RequestMapping(value = "usu12")
---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,40 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    //Crear la funcion para cargar los Usuarios
+    cargarUsuarios();
+
+  $('#usuarios').DataTable();
+});
+
+async function cargarUsuarios(){
+
+        //funcion fetch para llamar a un servidor
+
+      const request = await fetch('usuarios', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+      const usuarios = await request.json();
+
+       //variable para el listado del html
+       let listadoHtml = '';
+      for (let usuario of usuarios){
+
+            let usuarioHtml = '<tr><td>' + usuario.id + '</td><td>' + usuario.nombre + ' ' + usuario.apellido + '</td> <td>'
+                              + usuario.email + '</td><td>' + usuario.telefono
+                              + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"> </i></a></td></tr>';
+            listadoHtml += usuarioHtml;
+      }
+
+      console.log(usuarios);
+
+       //variable usuario
+
+
+   //codigo html para el body
+    document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+
+}
