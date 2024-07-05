539e168c1ac9277c4352194758980edf88a8263c
Daniel Núnez Rojas <invdesdelnr@outlook.com>
2024-07-05 16:14:22 -0400

    segundo commit

---
+++ src/main/java/com/practicaJava/practica/dao/UsuarioDao.java
@@ -0,0 +1,13 @@
+package com.practicaJava.practica.dao;
+
+
+
+import com.practicaJava.practica.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+
+    List<Usuario> getUsuarios();
+}
